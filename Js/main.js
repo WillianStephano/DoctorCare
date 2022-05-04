d4de536c1c5468031d24cd0e3590953a28296a30
Willian Stephano <will.stephano@gmail.com>
2022-05-04 18:42:44 -0300

    Criação de toggle menu em dispositivos moveis

--- Js/main.js
+++ Js/main.js
@@ -17,3 +17,31 @@ window.onscroll = function () {
    }
    
 }
+
+/* toggle menu */
+var btToggleMenu = document.querySelector('.bt_toggle_menu')
+
+
+btToggleMenu = addEventListener('click', function () {
+   rotacaoBarras()
+   mostrarMenu()
+
+ 
+   
+})
+
+function rotacaoBarras() {
+   const barra1 = document.querySelector('.bar_toggle_menu_1')
+   const barra2 = document.querySelector('.bar_toggle_menu_2')
+   const barra3 = document.querySelector('.bar_toggle_menu_3')
+
+   barra1.classList.toggle('rotate_1')
+   barra2.classList.toggle('rotate_2')
+   barra3.classList.toggle('rotate_3')
+}
+
+function mostrarMenu() {
+   const menu = document.querySelector('.menu_nav_cabecalho')
+
+   menu.classList.toggle('menu_ativo')
+}
